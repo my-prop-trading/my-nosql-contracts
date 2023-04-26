368931e6a1d6416254d8390e7ff4a92cab43384e
Aleksandr Belousov <aleksandr.b@simple.app>
2023-04-26 18:34:00 +0300

    add email templates

---
+++ src/email_template.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+use rust_extensions::StrOrString;
+use serde::{Deserialize, Serialize};
+
+#[my_no_sql_macros::my_no_sql_entity("email-template")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "PascalCase")]
+pub struct EmailTemplateMyNoSqlEntity {
+    pub template_name: String,
+    pub language: String,
+    pub external_id: String,
+    pub key_value: HashMap<String, String>,
+}
+
+impl EmailTemplateMyNoSqlEntity {
+    pub fn generate_partition_key(template_name: &str) -> &str {
+        template_name
+    }
+
+    pub fn generate_row_key<'s>(language: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
+        language.into()
+    }
+}
