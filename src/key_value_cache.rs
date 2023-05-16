ad9bf0b21a130f6a7a1da7cb6e8bb96fcbac6c78
Aleksandr Belousov <aleksandr.b@simple.app>
2023-05-16 19:04:10 +0300

    reverse

--- src/key_value_cache.rs
+++ src/key_value_cache.rs
@@ -1,40 +1,21 @@
+use std::collections::HashMap;
+
 use rust_extensions::StrOrString;
 use serde::{Deserialize, Serialize};
 
-#[my_no_sql_macros::my_no_sql_entity("trader-packages-field-order")]
+#[my_no_sql_macros::my_no_sql_entity("key-value-cache")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
-pub struct TraderPackageFieldsOrderMyNoSqlEntity {
-    pub phase1_daily_drawdown: Option<i32>,
-    pub phase1_overall_drawdown: Option<i32>,
-    pub phase1_target_profit: Option<i32>,
-    pub phase1_duration: Option<i32>,
-    pub phase1_min_trading_days: Option<i32>,
-    pub phase1_min_opened_positions: Option<i32>,
-    pub phase1_revenue_share: Option<i32>,
-    pub phase1_refund: Option<i32>,
-
-    pub phase2_daily_drawdown: Option<i32>,
-    pub phase2_overall_drawdown: Option<i32>,
-    pub phase2_target_profit: Option<i32>,
-    pub phase2_duration: Option<i32>,
-    pub phase2_min_trading_days: Option<i32>,
-    pub phase2_min_opened_positions: Option<i32>,
-    pub phase2_revenue_share: Option<i32>,
-    pub phase2_refund: Option<i32>,
-
-    pub daily_drawdown: Option<i32>,
-    pub overall_drawdown: Option<i32>,
-    pub target_profit: Option<i32>,
-    pub revenue_share: Option<i32>,
+pub struct KeyValueCacheMyNoSqlEntity {
+    pub key_value: HashMap<String, String>,
 }
 
-impl TraderPackageFieldsOrderMyNoSqlEntity {
+impl KeyValueCacheMyNoSqlEntity {
     pub fn generate_partition_key() -> &'static str {
         "c"
     }
 
-    pub fn generate_row_key<'s>(trader_package_id: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
-        trader_package_id.into()
+    pub fn generate_row_key<'s>(trader_id: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
+        trader_id.into()
     }
 }
