151ee5cd892ab6aa619ea4c6515ce67bd040fc5e
yuriy-smplt <yuriy.p@smplt.net>
2024-04-30 13:59:55 +0300

    updated

--- src/cfd_bridge/cfd_bridge_trader_cache.rs
+++ src/cfd_bridge/cfd_bridge_trader_cache.rs
@@ -6,14 +6,14 @@ use crate::cfd_bridge::cfd_account_type::CfdAccountTypeMyNoSql;
 #[my_no_sql_entity("cfd-bridge-trader-cache")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
-pub struct CfdBridgeCacheMyNoSqlEntity {
+pub struct CfdBridgeTraderCacheMyNoSqlEntity {
     pub client_id: String,
     pub trader_account_id: String,
     pub platform_account_id: String,
     pub account_type: CfdAccountTypeMyNoSql,
 }
 
-impl CfdBridgeCacheMyNoSqlEntity {
+impl CfdBridgeTraderCacheMyNoSqlEntity {
     pub fn generate_partition_key<'s>(client_id: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
         client_id.into()
     }
