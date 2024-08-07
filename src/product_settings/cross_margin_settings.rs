0231428c78255f0e3ce45bbad54b6592f3053748
Maxim Gorin <onlinexmax@gmail.com>
2024-08-07 17:57:36 +0300

    Add eq traits for ctrader, cross margin

--- src/product_settings/cross_margin_settings.rs
+++ src/product_settings/cross_margin_settings.rs
@@ -2,7 +2,7 @@ use serde::*;
 service_sdk::macros::use_my_no_sql_entity!();
 
 #[enum_model(partition_key:"trading-platform-bridge", row_key: "cross-margin")]
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "PascalCase")]
 pub struct CrossMarginSettingsModel {
     pub group_demo: String,
--- src/product_settings/ctrader_settings.rs
+++ src/product_settings/ctrader_settings.rs
@@ -2,7 +2,7 @@ use serde::*;
 service_sdk::macros::use_my_no_sql_entity!();
 
 #[enum_model(partition_key:"trading-platform-bridge", row_key: "ctrader")]
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 #[serde(rename_all = "PascalCase")]
 pub struct CtraderSettingsModel {
     pub demo_manager_login: i64,
