29dc78794d36825fa730100ddc587b5fd0e806c2
Aleksandr Belousov <aleksandr.b@simple.app>
2024-09-28 03:49:03 +0300

    Revert "https://fintechb2b.atlassian.net/browse/PROP2024-1154"
    This reverts commit cc35a24d7389b158e792c005eab3db4761abcf29.

--- src/trader_account_entity.rs
+++ src/trader_account_entity.rs
@@ -110,7 +110,6 @@ pub enum TraderAccountStatusMyNoSql {
     Disabled = 2,
     StageCompleted = 3,
     Blocked = 4,
-    TradingSuspended = 5,
 }
 
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq, Hash)]
