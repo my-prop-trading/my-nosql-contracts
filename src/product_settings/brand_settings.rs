303e5fb3b3041f4f672b13b345bca9eb06160579
yuriy-smplt <pluriw@gmail.com>
2024-10-30 09:18:10 +0200

    updated

--- src/product_settings/brand_settings.rs
+++ src/product_settings/brand_settings.rs
@@ -47,4 +47,5 @@ pub struct BrandSettingsModel {
     pub mail_success_picture_url: Option<String>,
     pub mail_fail_picture_url: Option<String>,
     // 
+    pub registration_type: Option<i64>,
 }
