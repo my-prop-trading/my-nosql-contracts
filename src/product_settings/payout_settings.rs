cea2868fc8e156a96316e3f1bd5675e30ba9bc3d
Maxim Gorin <onlinexmax@gmail.com>
2025-01-31 13:43:44 +0300

    Add is_bank_transfer_disabled

--- src/product_settings/payout_settings.rs
+++ src/product_settings/payout_settings.rs
@@ -8,10 +8,16 @@ service_sdk::macros::use_my_no_sql_entity!();
 pub struct PayoutSettingsModel {
     pub currencies: Vec<String>,
     pub cryptos: Vec<CryptoDescription>,
+    #[serde(default = "default_is_enable")]
+    pub is_bank_transfer_enabled: bool,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct CryptoDescription {
     pub crypto: String,
     pub networks: Vec<String>,
+}
+
+fn default_is_enable() -> bool {
+    true
 }
