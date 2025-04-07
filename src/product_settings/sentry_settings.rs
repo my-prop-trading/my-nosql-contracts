a584fc20536c3bef81dc6e62f54aaeccfdf60f16
MichaelR <m.redkin@fintechb2b.com>
2025-04-07 20:22:46 +0300

    Fix for prev.

--- src/product_settings/sentry_settings.rs
+++ src/product_settings/sentry_settings.rs
@@ -2,7 +2,7 @@ use serde::*;
 
 service_sdk::macros::use_my_no_sql_entity!();
 
-#[enum_model(partition_key:"settings", row_key: "sentry")]
+#[enum_model(partition_key:"affiliate", row_key: "sentry")]
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "PascalCase")]
 pub struct SentrySettingsModel {
