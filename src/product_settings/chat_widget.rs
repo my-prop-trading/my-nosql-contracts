108a3eeb1e49a4825ddbebeb1867044b7b7cbb04
MichaelR <m.redkin@fintechb2b.com>
2025-04-10 14:31:59 +0300

    Add chat_type.

--- src/product_settings/chat_widget.rs
+++ src/product_settings/chat_widget.rs
@@ -5,7 +5,8 @@ use serde::*;
 pub struct ChatWidget {
     pub live_chat: Option<ChatSetting>,
     pub zen_desk: Option<ChatSetting>,
-    pub hide_widget: bool,
+    pub hide_widget: Option<bool>,
+    pub chat_type: Option<u8>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
