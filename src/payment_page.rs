ecc38dac7b7812d1cdc7fff0e56f670aca349b7a
Maxim Gorin <onlinexmax@gmail.com>
2025-05-05 17:32:06 +0300

    Change payment page

--- src/payment_page.rs
+++ src/payment_page.rs
@@ -10,22 +10,16 @@ service_sdk::macros::use_my_no_sql_entity!();
 pub struct PaymentPageMyNoSqlEntity {
     pub expires: Timestamp,
     pub html: String,
+    pub order_id: String,
+    pub client_id: String,
 }
 
 impl PaymentPageMyNoSqlEntity {
-    pub fn generate_partition_key(client_id: impl Into<String>) -> String {
-        client_id.into()
+    pub fn generate_partition_key() -> String {
+        "*".into()
     }
 
     pub fn generate_row_key(order_id: impl Into<String>) -> String {
         order_id.into()
-    }
-
-    pub fn get_order_id(&self) -> &str {
-        &self.row_key
-    }
-
-    pub fn get_client_id(&self) -> &str {
-        &self.partition_key
     }
 }
