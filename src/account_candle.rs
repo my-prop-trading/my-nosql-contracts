d93571c3b44b7e4039b4850de8e28cf7f65611b6
Maxim Gorin <onlinexmax@gmail.com>
2025-05-16 18:40:03 +0300

    Change candle

--- src/account_candle.rs
+++ src/account_candle.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use service_sdk::my_no_sql_sdk::abstractions::Timestamp;
 use service_sdk::my_no_sql_sdk::macros::my_no_sql_entity;
 use service_sdk::rust_extensions::StrOrString;
 
@@ -9,16 +8,16 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
 pub struct AccountCandleMyNoSqlEntity {
-    pub expires: Timestamp,
     pub interval_date: String,
-    pub balance_data: CandleDataMyNoSqlEntity,
-    pub equity_data: CandleDataMyNoSqlEntity,
-    pub pnl_data: CandleDataMyNoSqlEntity,
+    pub interval: i32,
+    pub balance_data: AccountCandleDataMyNoSqlEntity,
+    pub equity_data: AccountCandleDataMyNoSqlEntity,
+    pub pnl_data: AccountCandleDataMyNoSqlEntity,
 }
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct CandleDataMyNoSqlEntity {
+pub struct AccountCandleDataMyNoSqlEntity {
     pub open: f64,
     pub close: f64,
     pub high: f64,
@@ -28,11 +27,11 @@ pub struct CandleDataMyNoSqlEntity {
 }
 
 impl AccountCandleMyNoSqlEntity {
-    pub fn generate_pk(candle_interval: i32) -> String {
-        candle_interval.to_string()
+    pub fn generate_rk<'s>(candle_id: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
+        candle_id.into()
     }
 
-    pub fn generate_rk<'s>(trader_account_id: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
+    pub fn generate_pk<'s>(trader_account_id: impl Into<StrOrString<'s>>) -> StrOrString<'s> {
         trader_account_id.into()
     }
 
@@ -41,6 +40,6 @@ impl AccountCandleMyNoSqlEntity {
     }
 
     pub fn get_candle_interval(&self) -> i32 {
-        self.partition_key.parse().unwrap()
+        self.row_key.parse().unwrap()
     }
 }
